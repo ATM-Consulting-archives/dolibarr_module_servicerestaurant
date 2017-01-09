ac74e1f61e56648cf837755294d6c55a6e007d14
Unknown <clement.guezel@iut-valence.fr>
2017-01-09 15:55:08 +0100

    Dependencies
    Add the dependencies of the module

--- core/modules/modservicerestaurant.class.php
+++ core/modules/modservicerestaurant.class.php
@@ -98,7 +98,7 @@ class modservicerestaurant extends DolibarrModules
 
 		// Dependencies
 		$this->hidden = false;			// A condition to hide module
-		$this->depends = array();		// List of modules id that must be enabled if this module is enabled
+		$this->depends = array("modCategorie","modCommande","modFacture","modStock","modProduct");		// List of modules id that must be enabled if this module is enabled
 		$this->requiredby = array();	// List of modules id to disable if this one is disabled
 		$this->conflictwith = array();	// List of modules id this module is in conflict with
 		$this->phpmin = array(5,0);					// Minimum version of PHP required by module
