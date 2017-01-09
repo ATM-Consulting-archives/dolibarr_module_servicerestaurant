f5c200a1ec36cc4f77f2cfb9797dfd6fc38c0ef3
Unknown <clement.guezel@iut-valence.fr>
2017-01-09 19:07:35 +0100

    Test Game v2
    Auto creation of game test for societe and user 
    including usergroup, rights ..

--- sql/data.sql
+++ sql/data.sql
@@ -7,6 +7,36 @@
  * Author:  Clément
  * Created: 9 janv. 2017
  */
+-- Création Utilisateurs*
+
+Insert into llx_usergroup(nom,entity,datec,tms,note)
+values("Serveurs",1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,"Les serveurs du restaurant");
+
+Insert into llx_user(entity,datec,tms,login,lastname,firstname,job)
+values(1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,"Serveur1","Test","Serveur","Serveur");
+
+Insert into llx_usergroup_user(entity,fk_user,fk_usergroup)
+values
+(
+    1,
+    (select rowid from llx_user where login="Serveur1"),
+    (select rowid from llx_usergroup where nom="Serveurs")
+);
+
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),11);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),12);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),31);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),81);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),82);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),87);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),121);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),122);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),241);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),342);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),343);
+Insert into llx_usergroup_rights(fk_usergroup,fk_id) values((select rowid from llx_usergroup where nom="Serveurs"),1001);
+
+-- Fin création utilisateurs
 
 -- Création des tables 
 insert into llx_categorie(entity,fk_parent,label,type,description,visible)
@@ -161,3 +191,4 @@ values
 );
 
 -- Fin création des tables
+
