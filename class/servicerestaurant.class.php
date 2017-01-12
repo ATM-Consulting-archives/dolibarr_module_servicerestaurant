4069ad49e2a401741616a797d025a1026e624137
Unknown <clement.guezel@iut-valence.fr>
2017-01-12 12:28:23 +0100

    NEW TestGameSocieteAndCategories and NEW TestGameProductAndCategorie
    this 2 method finish the test_game method

--- class/servicerestaurant.class.php
+++ class/servicerestaurant.class.php
@@ -2,10 +2,11 @@
 
 dol_include_once('/product/class/product.class.php');
 dol_include_once('/commande/class/commande.class.php');
-dol_include_once("/categories/class/categories.class.php");
+dol_include_once("/categories/class/categorie.class.php");
 dol_include_once("/user/class/user.class.php");
 dol_include_once("/societe/class/client.class.php");
 dol_include_once("/user/class/usergroup.class.php");
+dol_include_once("/product/stock/class/entrepot.class.php");
 
 class ControllerServiceRestaurant
 {
@@ -56,10 +57,14 @@ class ControllerServiceRestaurant
     
         function test_game()
         {
-            $id_group=$this->TestGameUserAndUsergroup($this->db); 
+            $admin=new User($this->db);
+            $admin->fetch(1);
+            $this->TestGameUserAndUsergroup($this->db,$admin);
+            $this->TestGameSocieteAndCategories($this->db,$admin);
+            $this->TestGameProductAndCategorie($this->db,$admin);
         }
 
-        function TestGameUserAndUsergroup($db)
+        function TestGameUserAndUsergroup($db,$admin)
         {
             $usergroup= new UserGroup($db);
             $usergroup->name="Serveurs";
@@ -79,7 +84,66 @@ class ControllerServiceRestaurant
             $user->firstname="test";
             $user->entity=1;
             $user->pass="";
-            $user->create($user);
+            $user->create($admin);
             $user->SetInGroup($id_group,1);
+        }
+        
+        function TestGameSocieteAndCategories($db,$admin)
+        {
+            $categorie=new Categorie($db);
+            $categorie->entity=1;
+            $categorie->fk_parent=0;
+            $categorie->label="Tables";
+            $categorie->type=2;
+            $categorie->description="Liste des tables du restaurant";
+            $categorie->visible=0;
+            $id_categorie=$categorie->create($admin);
+            
+            for ($i=1;$i<11;$i++)
+            {
+                $societe=new Societe($db);
+                $societe->nom="Table $i";
+                $societe->client=1;
+                $societe->create($admin);
+                $societe->setCategories($id_categorie, 'customer');
+            }
+        }
+        
+        function TestGameProductAndCategorie($db,$admin)
+        {
+            $entrepot=new Entrepot($db);
+            $entrepot->libelle="E1";
+            $entrepot->description="Entrepot 1";
+            $entrepot->statut=1;
+            $entrepot->country_id=1;
+            $id_entrepot=$entrepot->create($admin);
+            
+            $categProduct=array(
+                array("Entrees", "Les entrees du restaurant", "E", "Entree"),
+                array("Plats", "Les plats du restaurant", "P" ,"Plat"),
+                array("Desserts", "Les desserts du restaurant", "D" ,"Dessert")
+            );
+            foreach ($categProduct as $categName)
+            {
+                $categorie=new Categorie($db);
+                $categorie->entity=1;
+                $categorie->fk_parent=0;
+                $categorie->label=$categName[0];
+                $categorie->type=0;
+                $categorie->description=$categName[1];
+                $categorie->visible=0;
+                $id_categorie=$categorie->create($admin);
+                
+                for ($i=1;$i<4;$i++)
+                {
+                    $product=new Product($db);
+                    $product->ref="$categName[2]$i";
+                    $product->stock=10;
+                    $product->label="$categName[3] $i";
+                    $product->create($admin);
+                    $product->setCategories($id_categorie);
+                    $product->correct_stock($admin, $id_entrepot, 10, 0);
+                }
+            }
         }
 }
